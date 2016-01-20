84235128e46d95ec1dc3cd5f4d91cfac64cb92e1
cornixxx <nastya.cornix@gmail.com>
2016-01-21 00:31:08 +0200

    header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+/**
+ * Created by nasty on 01.01.2016.
+ */
+
+// Can also be used with $(document).ready()
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+});
+
+
 
